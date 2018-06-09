a04ba7526052f1dc193d5d7dd35a051ff5971ac1
forrest venable <forrest.venable@gmail.com>
2018-06-09 17:33:02 -0600

    folded element_manager into element

--- models/element.py
+++ models/element.py
@@ -1,13 +1,36 @@
 class Element:
-	def __init__(selenium_element):
-		this.selenium_element = selenium_element
-		pass
+	def __init__(name, description, selector, driver):
+		this.name = name
+		this.description = description
+		this.selector = selector
+		this.driver = driver
+		this._selenium_element = None
 
 	def input(string):
-		elem = this.selenium_element
-		assert self.is_displayed()
+		elem = _lazy_load()
+		assert this.is_displayed()
 		elem.send_keys(string)
 
-	def is_displayed():
+	def click:
+		elem = _lazy_load()
+		assert this.is_displayed()
+		elem.click()
+
+	def is_displayed:
+		elem = _lazy_load()
 		elem = this.selenium_element
 		return elem.displayed
+
+	def _lazy_load:
+		elem = this._selenium_element
+		if(!elem):
+			driver = this.driver
+			if(this.selector[0] == "."):
+				elem = driver.find_element_by_class(this.selector[1:])
+			else if(this.selector[0] == "#"):
+				elem = driver.find_element_by_id(this.selector[1:])
+			else:
+				elem = driver.find_element_by_name(this.selector)
+		return elem
+
+
