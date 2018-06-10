928d7131951b6dc8f64c330ee8bec16c2e2f6430
forrest venable <forrest.venable@gmail.com>
2018-06-10 02:43:32 -0600

    change find logic to by css selector

--- models/element.py
+++ models/element.py
@@ -7,30 +7,25 @@ class Element:
 		this._selenium_element = None
 
 	def input(string):
-		elem = _lazy_load()
+		elem = _lazy_find()
 		assert this.is_displayed()
 		elem.send_keys(string)
 
 	def click:
-		elem = _lazy_load()
+		elem = _lazy_find()
 		assert this.is_displayed()
 		elem.click()
 
 	def is_displayed:
-		elem = _lazy_load()
+		elem = _lazy_find()
 		elem = this.selenium_element
 		return elem.displayed
 
-	def _lazy_load:
+	def _lazy_find:
 		elem = this._selenium_element
 		if(!elem):
 			driver = this.driver
-			if(this.selector[0] == "."):
-				elem = driver.find_element_by_class(this.selector[1:])
-			else if(this.selector[0] == "#"):
-				elem = driver.find_element_by_id(this.selector[1:])
-			else:
-				elem = driver.find_element_by_name(this.selector)
+			elem = driver.find_element_by_css_selector(this.selector)
 		return elem
 
 
