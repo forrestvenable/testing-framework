cd9527fa89a09db8f7fc6da70773d23f78e62c8c
forrest venable <forrest.venable@gmail.com>
2018-06-11 15:16:57 -0600

    database is no longer a class

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,28 +1,30 @@
 import unittest
 from models import page, element, database, driver
-from selenium.webdriver.common.keys import Keys
 
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
         driver.connect()
-        db = database.Database
+        db = database
         db.connect()
         db.insert_page("Homepage","http://www.python.org",)
         db.insert_element("Search", "Search bar", "#id-search-field")
+        db.insert_element("SearchButton", "Search bar button", ".search-button")
 
     def test_search_in_python_org(self):
         db.find_page("Homepage").goto()
-        elem = db.find_element("Search")
-        elem.input("pycon")
-        elem.input(Keys.RETURN)
-        assert "No results found." not in self.driver.page_source
+        search = db.find_element("Search")
+        search.input("pycon")
+        button = db.find_element("SearchButton")
+        button.click()
+        assert "No results found." not in driver.page_source
 
 
     def tearDown(self):
         driver.disconnect()
         db.delete_page("Homepage")
         db.delete_page("Search")
+        db.delete_page("SearchButton")
         db.disconnect()
 
 if __name__ == "__main__":
