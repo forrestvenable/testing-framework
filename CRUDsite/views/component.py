f4c280c10eceb48dea9de065ea00b247388b2e7a
forrest venable <forrest.venable@gmail.com>
2018-06-22 16:18:02 -0600

    add views remove serializer

---
+++ CRUDsite/views/component.py
@@ -0,0 +1,24 @@
+from models import db
+from django.http import HttpResponse
+
+def create(request):
+    cursor = db.connection.cursor()
+    # INSERT QUERY HERE
+    db.connection.commit()
+    # INSERT HTML HERE
+
+def show(request):
+    cursor = db.connection.cursor()
+    # INSERT QUERY HERE
+    # INSERT HTML HERE
+
+def index(request):
+    cursor = db.connection.cursor()
+    # INSERT QUERY HERE
+    # INSERT HTML HERE
+
+def delete(request):
+    cursor = db.connection.cursor()
+    # INSERT QUERY HERE
+    db.connection.commit()
+    # INSERT HTML HERE
